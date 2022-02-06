9b9d9295ba79376e5e5d27dc0a010aa67b1700fe
Davi Borges <davifelipe_300@hotmail.com>
2022-02-06 13:23:54 -0300

    Exercicio para entender o comportamento de laços internos e laços externos

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ExercicioDecrementoNumerosRepeticao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valorMaximo;
+            int contadorExterno = 0;
+            int contadorInterno = 1;
+
+            Console.WriteLine("Digite o numero inicial da contage: ");
+            valorMaximo = Convert.ToInt32(Console.ReadLine());
+
+            while (contadorExterno < valorMaximo)
+            {   
+                Console.WriteLine();
+                while(contadorInterno <= valorMaximo - contadorExterno)
+                {
+                Console.Write(contadorInterno + " ");
+                contadorInterno = contadorInterno + 1;
+                }
+                contadorExterno = contadorExterno + 1;
+                contadorInterno = 1;
+            }
+        }
+    }
+}
